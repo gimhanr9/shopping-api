d0213435cb69b80a833efda3591e74ee58133db0
Gimhan Rodrigo <gimhanrg@gmail.com>
2023-02-16 19:19:12 +0530

    refactor schemas

--- models/purchase.js
+++ models/purchase.js
@@ -4,14 +4,18 @@ const Schema = mongoose.Schema;
 const PurchaseSchema = new Schema(
   {
     userId: { type: String, required: true },
-    productId: { type: String, required: true },
-    productImageUrl: { type: String, required: true },
-    productName: { type: String, required: true },
-    productSize: { type: String, required: true },
-    quantity: { type: Number, required: true },
-    price: { type: Number, required: true },
-    paymentMethod: { type: String, required: true },
-    rated: { type: Boolean, required: true },
+    purchases: [
+      {
+        productId: { type: String, required: true },
+        productImageUrl: { type: String, required: true },
+        productName: { type: String, required: true },
+        productSize: { type: String, required: true },
+        quantity: { type: Number, required: true },
+        price: { type: Number, required: true },
+        paymentMethod: { type: String, required: true },
+        rated: { type: Boolean, required: true },
+      },
+    ],
   },
   { timestamps: true }
 );
